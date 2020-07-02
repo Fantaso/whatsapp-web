83f46ca2d834c1d4fabe8c054a398466a6fcfe88
Carlos Rosas <fantaso.code@gmail.com>
2020-07-02 13:34:48 +0200

    [app:refactor] rename main.py to tests.py and moved it 1 directory up: better structure

--- simon/page.py
+++ simon/page.py
@@ -1,5 +1,5 @@
-from element import BasePageElement
-from locators import MainPageLocators
+from .element import BasePageElement
+from .locators import MainPageLocators
 
 
 class SearchTextElement(BasePageElement):
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 from selenium import webdriver
-import page
+from simon import page
 
 
 class PythonOrgSearch(unittest.TestCase):
