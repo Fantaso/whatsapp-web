cfcda14bdfc0d8f14e226cb86dd7e084b0d3d334
Carlos Rosas <fantaso.code@gmail.com>
2020-07-07 19:34:23 +0200

    [app:refactor] updated PanePage or OpenedChats page -remove all previous test and implemented a new multi element for the opened chats section

--- simon/tests/pane_page_tests.py
+++ simon/tests/pane_page_tests.py
@@ -1,57 +1,78 @@
 import time
 import unittest
-from pprint import pprint
 from unittest import TestCase
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 from simon import pages
+from simon.locators import ChatLocators
 
 
-class BaseFeatureEins:
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.get("https://web.whatsapp.com/")
-        self.login_page = pages.LoginPage(self.driver)
-        time.sleep(12)
+class PaneBaseTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Firefox()
+        cls.driver.maximize_window()
 
-    def tearDown(self):
-        # time.sleep(10)
-        self.driver.quit()
+        # Manually login in
+        cls.login_page = pages.LoginPage(cls.driver)
+        cls.login_page.load()
+        time.sleep(7)
+
+        cls.pane_page = pages.PanePage(cls.driver)
+        cls.pane_page.load()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 
-class FeatureEins(BaseFeatureEins, TestCase):
-    """Can detect new message(s), read them, analyse it and reply if needed."""
+class PanePageTests(PaneBaseTestCase):
+    def test_can_get_opened_chats(self):
+        self.assertGreaterEqual(len(self.pane_page.opened_chats), 5)
 
-    def test_can_find_opened_chats_section(self):
-        opened_chats = pages.PanePage(self.driver)
-        self.assertTrue(opened_chats.is_pane_page_available())
-        pprint(opened_chats.get_opened_chats_contacts())
+    def test_can_click_in_the_first_opened_chat(self):
+        first_chat = self.pane_page.opened_chats[0]
+        first_chat.click()
+        self.assertIsNotNone(self.driver.find_element(*ChatLocators.CHAT))
 
-    def test_can_find_first_contact_with_new_message(self):
-        opened_chats = pages.PanePage(self.driver)
-        self.assertTrue(opened_chats.is_new_message_available())
+    def test_can_get_first_opened_chat_details(self):
+        first_chat = self.pane_page.opened_chats[0]
+        self.assertIsNotNone(first_chat.name)
+        self.assertIsNotNone(first_chat.icon)
+        self.assertIsNotNone(first_chat.last_message)
+        self.assertIsNotNone(first_chat.last_message_time)
 
-    def test_can_find_new_message_and_click_to_go_to_contact_chat(self):
-        opened_chats = pages.PanePage(self.driver)
-        # get all opened chats available
-        # get attr (nombre, last msg, double arrow, truncated, new msg)
-        # if new_message: click to go tchat
+    def test_can_iterate_over_first_five_opened_chats(self):
+        for opened_chat in self.pane_page.opened_chats[:5]:
+            self.assertIsNotNone(opened_chat.name)
+            self.assertIsNotNone(opened_chat.icon)
+            self.assertIsNotNone(opened_chat.last_message)
+            self.assertIsNotNone(opened_chat.last_message_time)
 
-        # get first new msg
-        new_msg = opened_chats.get_new_message()
-        new_msg.click()
+    def test_can_detect_first_opened_chat_with_new_messages(self):
+        chat = self.pane_page.get_first_opened_chat_with_notifications()
+        self.assertIsInstance(chat.notifications, int)
+        self.assertGreaterEqual(chat.notifications, 1)
 
-        ## read the mesage
+    def test_can_detect_first_opened_chat_with_new_messages_and_click_it(self):
+        chat = self.pane_page.get_first_opened_chat_with_notifications()
+        self.assertGreaterEqual(chat.notifications, 1)
+        chat.click()
+        self.assertIsNotNone(self.driver.find_element(*ChatLocators.CHAT))
+        self.assertIn("unread message", self.driver.page_source.lower())
 
-        # check that the chat had opended
-        chat_page = pages.ChatPage(self.driver)
-        # get text elememnt
-        # write to texxt element
-        # keys.RETUNR to send message
-
-    def test_can_send_a_msg_to_a_contact(self):
-        contact_name = "Rastri"
+    def test_can_detect_first_opened_chat_with_new_messages_and_send_a_message(self):
+        """
+        This a manual test. It will be removed when
+        the elements are created for the ChatPage.
+        """
+        chat = self.pane_page.get_first_opened_chat_with_notifications()
+        chat.click()
+        chat_text_field = self.driver.find_element(*ChatLocators.TEXT_FIELD)
+        chat_text_field.send_keys("Hi, I am Simon: Carlos's WhatsApp ChatBot!")
+        chat_text_field.send_keys(Keys.RETURN)
 
 
 if __name__ == "__main__":
