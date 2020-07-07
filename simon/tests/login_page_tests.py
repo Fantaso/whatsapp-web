17fd911dd7b9a29a77361ec0cf21d215211b4fc2
Carlos Rosas <fantaso.code@gmail.com>
2020-07-07 19:19:49 +0200

    [app:refactor] updated login tests - using a load method instead of a simple driver.get() - added a new test to check to toggle back on and off the rememeber checkbox - using a maximaize window now - in teardown now we driver.quit() instead of driver.close(). it was causing some traceback problem

--- simon/tests/login_page_tests.py
+++ simon/tests/login_page_tests.py
@@ -7,18 +7,19 @@ from selenium import webdriver
 from simon import pages
 
 
-class BaseLoginTests:
+class LoginBaseTestCase(TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.get("https://web.whatsapp.com/")
+        self.driver.maximize_window()
         self.login_page = pages.LoginPage(self.driver)
+        self.login_page.load()
 
     def tearDown(self):
-        self.driver.quit()
+        self.driver.close()
 
 
-class LoginPageTests(BaseLoginTests, TestCase):
-    def test_can_open_whatsapp_web(self):
+class LoginPageTests(LoginBaseTestCase):
+    def test_can_open_whatsapp(self):
         self.assertTrue(self.login_page.is_title_matches())
         self.assertTrue(self.login_page.is_instruction_title_matches())
 
@@ -29,16 +30,22 @@ class LoginPageTests(BaseLoginTests, TestCase):
         self.login_page.remember_me = False
         self.assertFalse(self.login_page.is_remember_me_selected())
 
+    def test_can_uncheck_and_check_again_checkbox_remember_me(self):
+        self.login_page.remember_me = False
+        self.assertFalse(self.login_page.is_remember_me_selected())
+        self.login_page.remember_me = True
+        self.assertTrue(self.login_page.is_remember_me_selected())
 
-class ManualLoginTests(BaseLoginTests, TestCase):
-    def test_can_login_successfully(self):
+    def test_can_manually_login_successfully(self):
         base_page = pages.BasePage(self.driver)
+        base_page.load()
         # time for you to read QR code and access whatsapp
-        time.sleep(10)
+        time.sleep(8)
         self.assertTrue(base_page.is_title_matches())
         self.assertTrue(base_page.is_welcome_page_available())
         self.assertTrue(base_page.is_nav_bar_page_available())
         self.assertTrue(base_page.is_search_page_available())
+        self.assertTrue(base_page.is_pane_page_available())
         # chat is only available after you click on a person to open the chat
         self.assertFalse(base_page.is_chat_page_available())
 
