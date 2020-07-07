c50f82e8f617a81d5becdc6d3b8eaeabb746a237
Carlos Rosas <fantaso.code@gmail.com>
2020-07-07 19:49:20 +0200

    [template] added comments for possible new tests: for ChatPage

---
+++ simon/tests/chat_page_tests.py
@@ -0,0 +1,14 @@
+# can iterate over all message in the proper order of time arrival
+# can read last message
+# can read last 5 messages
+# can read first message available
+# can read first 5 messages available
+# can send a message
+# can send a multiline message
+# can detect if the current chat is a fresh chat created by the contact or client
+
+# can detect all unread messages
+# can get the time from an unread message
+# can iterate over all unread messages in the proper order of time arrival
+
+# can reply to a specific message
